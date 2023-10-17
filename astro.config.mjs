ad3e4fce4073ef6193616e2dadd173b3c9bf8b96
teq-daihoang <daihoang@teqnological.asia>
2023-10-17 16:27:34 +0700

    wait

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,6 @@ export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap(), react()],
 	image: {
-		domains: ["avatars.githubusercontent"],
+		domains: ["avatars.githubusercontent", "raw.githubusercontent.com"],
 	  }
 });
